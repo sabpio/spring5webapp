1fc477112c5483c292b5c9f253cc3fe2de96120b
Piotr Sabacinski <sabacinski.piotr@gmail.com>
2020-04-19 15:45:14 +0200

    added Publisher

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,14 +14,22 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        System.out.println("Started in bootstrap..");
+
+        Publisher publisher = new Publisher("publisher", "armii krajowej 35", "piotrkow", "lodzkie", "97-300");
+        publisherRepository.save(publisher);
+
+        System.out.println("Number of publishers: " + publisherRepository.count());
 
         Author eric = new Author("eric", "evans");
         Book ddd = new Book("ddd", "123124124");
@@ -37,7 +47,6 @@ public class BootstrapData implements CommandLineRunner {
         rob.getBooks().add(noEJB);
         noEJB.getAuthors().add(rob);
 
-        System.out.println("Started in bootstrap..");
         System.out.println("Number of books = " + bookRepository.count());
     }
 }
